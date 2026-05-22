258716c7147237f2d6bd095f2f057b4055e8757b
Antonio Di Giulio <52922233+vodketto@users.noreply.github.com>
2026-05-22 11:36:49 +0200

    Set base path for deployment to '/Neon-Dungeon/'

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/Neon-Dungeon/",
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
